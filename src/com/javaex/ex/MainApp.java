6c8715de7806c32b9b6f04a480c2adebcba643a1
ahah2025 <adghty61@naver.com>
2025-05-20 12:16:36 +0900

    2025-05-20

--- src/com/javaex/ex/MainApp.java
+++ src/com/javaex/ex/MainApp.java
@@ -6,11 +6,18 @@ import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class MainApp {
 
 	public static void main(String[] args) throws IOException {
 
+		Person p = new Person();
+		System.out.print(p.toString());
+		
+		Scanner sc = new Scanner(System.in); 
+		sc.nextLine();
+		
 		//리스트 만들기
 		List<Person> pList = new ArrayList<Person>();
 		
@@ -21,37 +28,22 @@ public class MainApp {
 		while(true) {
 			String s = br.readLine();
 			
-			//마지막이면 끝
-			if(s == null) {
+			if(s == "1") {
+				System.out.print(p.toString());
 				break;
 			}
-			System.out.println(s);
-			
-			String[] sArr = s.split(","); 
-			
-			String name = sArr[0];
-			String hp = sArr[1];
-			String company = sArr[2];
-		
-			//Person을 메모리에 올린다
-			Person p = new Person(name,hp,company);
-			
-			//Person 주소를 List에 추가한다
-			pList.add(p);
 		}
-		
 		//출력
 		System.out.println("------출력------------------------");
-		//System.out.println(pList.toString());
+		System.out.println(pList.toString());
 		
-		System.out.println("첫번째 사람의 이름,hp,company");
-		
-		Person p01 = pList.get(1);
-		System.out.println(pList.get(1).getName());
-		System.out.println(pList.get(1).getHp());
-		System.out.println(pList.get(1).getCompany());
+		System.out.println("------첫번째 사람의 이름,hp,company--------------");
+		System.out.println(pList.get(0).getName());
+		System.out.println(pList.get(0).getHp());
+		System.out.println(pList.get(0).getCompany());
 		
 		//스트림 닫기
+		sc.close();
 		fr.close();
 		br.close();
 	}
