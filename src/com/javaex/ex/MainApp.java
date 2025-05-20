7e92a62f21a319c3ad3a9f13b7a67384fa7f9db7
ahah2025 <adghty61@naver.com>
2025-05-20 11:00:10 +0900

    2025-05-20

--- src/com/javaex/ex/MainApp.java
+++ src/com/javaex/ex/MainApp.java
@@ -4,35 +4,52 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainApp {
 
 	public static void main(String[] args) throws IOException {
 
-		/*
-		*리스트의 정보를 변경하고 모든 정보를 파일에 다시쓴다
-		arrayList(메모리)-->phoneDB.txt
-		 */
+		//리스트 만들기
+		List<Person> pList = new ArrayList<Person>();
 		
-		Person p = new Person();
-		System.out.println(p.toString());
-		
-		//주 Stream 준비
+		//Stream 준비
 		Reader fr = new FileReader("C:\\JavaStudy\\workspace\\Phonebook\\PhoneDB.txt");
-				
-		//보조 스트림 준비
 		BufferedReader br =new BufferedReader(fr);
 		
 		while(true) {
 			String s = br.readLine();
 			
-			if(s == "1") {
-				System.out.println("<1.리스트>"+"\n"+"1.	"+" 이효리	"+"010-2222-3333	"+"031-2323-4441");
+			//마지막이면 끝
+			if(s == null) {
 				break;
 			}
 			System.out.println(s);
+			
+			String[] sArr = s.split(","); 
+			
+			String name = sArr[0];
+			String hp = sArr[1];
+			String company = sArr[2];
+		
+			//Person을 메모리에 올린다
+			Person p = new Person(name,hp,company);
+			
+			//Person 주소를 List에 추가한다
+			pList.add(p);
 		}
 		
+		//출력
+		System.out.println("------출력------------------------");
+		//System.out.println(pList.toString());
+		
+		System.out.println("첫번째 사람의 이름,hp,company");
+		
+		Person p01 = pList.get(1);
+		System.out.println(pList.get(1).getName());
+		System.out.println(pList.get(1).getHp());
+		System.out.println(pList.get(1).getCompany());
 		
 		//스트림 닫기
 		fr.close();
