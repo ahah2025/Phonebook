544c301bd0dff45763053b92a1116ea578e96206
ahah2025 <adghty61@naver.com>
2025-05-20 14:50:42 +0900

    2025-05-20

--- src/com/javaex/ex/MainApp.java
+++ src/com/javaex/ex/MainApp.java
@@ -1,6 +1,9 @@
 package com.javaex.ex;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -10,7 +13,6 @@ public class MainApp {
 	public static void main(String[] args) throws IOException {
 
 		Person p = new Person();
-		System.out.print(p.toString());
 		
 		//리스트 만들기
 		List<Person> pList = new ArrayList<Person>();
@@ -26,44 +28,16 @@ public class MainApp {
 				System.out.println(pList.toString());
 				System.out.println();
 			
-				//메뉴번호 선택
-				String no = sc.nextLine(); 
-				switch(no) {
-					case "1": 
-						System.out.println(p.getName());
-						break;
-					case "2": 
-						System.out.println(p.getHp());
-						break;
-					case "3": 
-						System.out.println(p.getCompany());
-						break;
-					default:
-						System.out.println("파일이 없습니다");
-						break;
+				String a = sc.nextLine(); 
+				if(a == 1) {
+					System.out.println(p.toString());
+				}else if(a == 2) {
+					
 				}
 			}
-		/*
-		//Stream 준비
-		Reader fr = new FileReader("C:\\JavaStudy\\workspace\\Phonebook\\PhoneDB.txt");
-		BufferedReader br =new BufferedReader(fr);
-		
-		while(true) {
-			String i = sc.nextLine();
-			
-			if(i=="1") {
-				System.out.print("파일이 있습니다");
-				System.out.println(fr.read());
-				break;
-			}else {
-				System.out.print("파일 X");
-			}
-		}
-		
 		//스트림 닫기
-		sc.close();
 		fr.close();
 		br.close();
-		*/
+		sc.close();
 	}
 }
